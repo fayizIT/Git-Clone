68b8b38904f0b6c330ce73be77a395e556e0c11c
Fayiz <fayizcj94.com>
2023-12-19 21:05:37 +0530

    Navbar Arranging'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,44 @@
 // import React from 'react';
 import { Link } from "react-router-dom";
+import { FaBell } from 'react-icons/fa';
+import { FiPlus } from 'react-icons/fi';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import github from '../assets/github.png'
+
+
 
 const Navbar = () => {
   return (
-   <>
-   <h1>navbarrrr</h1>
-   </>
+   <nav className="navbar">
+
+    <div className="navleft">
+    <img src={github} alt="Website logo" />
+   
+   
+{/* left */}
+   <div>
+    <input type="text" placeholder="Search or jumb"/>
+   </div>
+   </div>
+
+   {/* center */}
+   <div className="navCenter">
+    <h2>pulls</h2>
+    <h2>Isues</h2>
+    <h2>MarketPlace</h2>
+    <h2>Explore</h2>
+   </div>
+
+   {/* Right */}
+
+   <div className="navRight">
+<FaBell/>
+<div className="navRightIcons">
+  <FiPlus/>
+  <IoMdArrowDropdown/>
+</div>
+</div>
+   </nav>
   );
 };
 
