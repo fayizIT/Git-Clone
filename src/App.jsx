f5a8a3a1bb96797da81c104027f84720d4196343
Fayiz <fayizcj94.com>
2023-12-19 18:06:04 +0530

    React vite latest Install and Run

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+
+
+  return (
+    <>
+    
+     <h1>Welcome to git Hub page</h1>
+     <Navbar/>
+    
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+   <>
+   <h1>navbarrrr</h1>
+   </>
+  );
+};
+
+export default Navbar;
